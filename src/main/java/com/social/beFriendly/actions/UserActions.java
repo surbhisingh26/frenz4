eb6ad36b21a6d1fe8b7c034253421b2a96d68855
slighttransformation <slighttransformation@outlook.com>
2018-06-01 00:51:47 +0530

    Paths changed at home

--- src/main/java/com/social/beFriendly/actions/UserActions.java
+++ src/main/java/com/social/beFriendly/actions/UserActions.java
@@ -34,7 +34,7 @@ public class UserActions extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	Utility utility = new Utility();
 	ObjectId uid;
-	String templatePath = "C:/soft/apache-tomcat-8.5.23/webapps/beFriendly/WEB-INF/templates/fancy-colorlib";
+	String templatePath = "D:/apps/apache-tomcat-8.5.5/webapps/beFriendly/WEB-INF/templates/fancy-colorlib";
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
