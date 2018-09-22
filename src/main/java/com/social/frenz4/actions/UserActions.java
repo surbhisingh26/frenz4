aefb13b120a96c17c1ac773a2b471ea139231ee8
slighttransformation <slighttransformation@outlook.com>
2018-09-22 17:38:25 +0530

    test

--- src/main/java/com/social/frenz4/actions/UserActions.java
+++ src/main/java/com/social/frenz4/actions/UserActions.java
@@ -69,7 +69,7 @@ public class UserActions extends HttpServlet {
 
 
 		String path = request.getPathInfo();
-		System.out.println("pat "+ path);
+		System.out.println("path "+ path);
 
 
 		if(path==null||path.equals("/")){
